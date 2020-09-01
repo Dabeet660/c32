dba3768b8b74c6171cc235febf42672cc90d7ac7
DabeetDas <46596266+DabeetDas@users.noreply.github.com>
2020-09-01 16:53:16 +0530

    Commit c32

--- Pig.js
+++ Pig.js
@@ -2,7 +2,7 @@ class Pig extends BaseClass {
   constructor(x, y){
     super(x,y,50,50);
     this.image = loadImage("sprites/enemy.png");
-    this.Visiblity = 255;
+    this.Visiblity = 255; // -3 || +1
   }
 
  display(){
@@ -21,6 +21,10 @@ class Pig extends BaseClass {
    
  }
 
-
+ score(){
+   if(this.Visiblity < 0 && this.Visiblity > -1005){
+     score++
+   }
+ }
 
 };
